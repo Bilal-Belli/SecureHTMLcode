3f73a7444ea248b738e64e6701ff571aefe866ac
Bilal <jb_belli@esi.dz>
2022-08-10 20:10:09 +0100

    i just finish, need maybe to add some complexity and a key for looping the escape

--- function.js
+++ function.js
@@ -2,6 +2,7 @@ let NormalCode = '';
 let resultHexEncoded = '';
 
 function encryptHTML(){
+    NormalCode = '';
     NormalHTMLCode = document.getElementById("htmlCode").value;
     resultHexEncoded = '';
     resultHexEncoded += '<script language="javascript">document.write(unescape(\'';
@@ -13,18 +14,38 @@ function encryptHTML(){
     document.getElementById("resultOfFunction").innerHTML = resultHexEncoded;
 }
 
+function decryptHTML(){
+    NormalCode = '';
+    NormalCode = document.getElementById("htmlCode").value;
+    NormalCode = NormalCode.replace('<script language="javascript">document.write(unescape(\'','');
+    NormalCode = NormalCode.replace('\'));</script>','');
+
+    resultHexEncoded = unescape(NormalCode);  
+    document.getElementById("resultOfFunction").innerHTML = resultHexEncoded;
+}
+
 function fetchEncriptFunction(){
+    let output = "";
+    document.getElementById("labelCodeType").innerHTML = `HTML Code` ;
+    document.getElementById("labelResult").innerHTML = `Result (Encoded HTML)` ;
 
+    output += `<textarea class="form-control" id="htmlCode" oninput="encryptHTML()" rows="6"></textarea>`;
+    document.querySelector('.form-outline.form-white.mb-4.firstOPdiv').innerHTML = `<textarea class="form-control" id="htmlCode" oninput="encryptHTML()" rows="6"></textarea>`;
+    
+    output = "";
+    output += `<textarea class="form-control" id="resultOfFunction" rows="6"></textarea>`;
+    document.querySelector('.form-outline.form-white.secondOPdiv').innerHTML = `<textarea class="form-control" id="resultOfFunction" readonly rows="6"></textarea>`;
 }
+
 function fetchDecriptFunction(){
+    let output = "";
+    document.getElementById("labelCodeType").innerHTML = `Encrypted Code` ;
+    document.getElementById("labelResult").innerHTML = `Result (HTML Code)| note: it decrypt one time` ;
 
+    output += `<textarea class="form-control" id="htmlCode" oninput="decryptHTML()" rows="6"></textarea>`;
+    document.querySelector('.form-outline.form-white.mb-4.firstOPdiv').innerHTML = output;
+
+    output = "";
+    output += `<textarea class="form-control" id="resultOfFunction" readonly rows="6"></textarea>`;
+    document.querySelector('.form-outline.form-white.secondOPdiv').innerHTML = output;
 }
-
-// function decryptHTML(){
-//     resultHexEncoded = document.getElementById("resultOfFunction").value;
-//     NormalCode = '';
-//     resultHexEncoded -= 
-//     NormalCode = unescape(resultHexEncoded);
-//     document.getElementById("htmlCode").innerHTML = NormalCode;
-//     console.log(unescape(hexEncoded));
-// }
